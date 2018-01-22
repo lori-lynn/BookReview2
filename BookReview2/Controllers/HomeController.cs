022371747eee906e5d2ed8821a6aaff8bf0218ff
lori-lynn <35235309+lori-lynn@users.noreply.github.com>
2018-01-22 11:53:53 -0800

    changes to bookreview

--- BookReview2/Controllers/HomeController.cs
+++ BookReview2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookReview2.Models;
 
 namespace BookReview2.Controllers
 {
@@ -10,7 +11,12 @@ namespace BookReview2.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            BookReviewDbEntities db = new BookReviewDbEntities();
+            //var bks = (from b in db.Books
+                     // select b).ToList();
+                     
+
+            return View(db.Books.ToList());
         }
 
         public ActionResult About()
